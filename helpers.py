de255ff5ff679985c5998d9f54647f778eb8c1f6
brad <34373485+baxtrax@users.noreply.github.com>
2024-04-25 15:19:04 -0400

    up to date

--- helpers.py
+++ helpers.py
@@ -1,7 +1,12 @@
+import sys
 import numpy as np
 import torch
+import math
+import tqdm
 
-FUNCTIONS = {'x^2': lambda x: x**2,}
+FUNCTIONS = {'x^(1.2)': lambda x: x**1.2,
+             'x^2': lambda x: x**2,
+             'x^3-6x^2+8x': lambda x: x**3 - 6*x**2 + 8*x,}
 
 def generate_function_data(func, num_samples, sample_step):
     """
@@ -15,7 +20,9 @@ def generate_function_data(func, num_samples, sample_step):
     Returns:
         A tuple containing two lists: the x values and the corresponding function values.
     """
+    # TODO convert to linspace, and changei nputs around
     x_values = np.arange(0, num_samples * sample_step, sample_step)
+    # x_values = np.linspace(0, num_samples * sample_step, num_samples)
     f_values = func(x_values)
     return x_values, f_values
 
@@ -26,7 +33,7 @@ def get_deriv_approx(x_values, func, n_deriv):
     function, which computes the partial gradient of a function with respect to 
     its inputs. By specifying grad_outputs interally to be ones_like(y), we are
     calculating the partial gradient across ALL inputs, effectively outputting 
-    a full derivative.
+    a full derivative. Technically uses finite differences.
 
     Parameters:
         x_values (list or numpy array): The x values at which to compute the derivative.
@@ -54,3 +61,88 @@ def get_deriv_approx(x_values, func, n_deriv):
     derivative_values = derivatives.detach().numpy()
     
     return derivative_values
+
+########## GL method https://www.youtube.com/watch?v=opJVMMPb-EI ##########
+
+def get_frac_deriv_approx_GL(x_values, func, n_deriv, h):
+    """
+    Computes the nth derivative of a function using the generalized GL formula.
+
+    Args:
+        x_values (list): List of x values to compute the derivative at.
+        func (function): Function to compute the derivative of.
+        n_deriv (int): Order of the derivative to compute. Also known as alpha
+        h (float): Step size for the approximation.
+
+    Returns:
+        list: List of nth derivative values for each x value in x_values.
+
+    Formula: 
+        ![image](https://quicklatex.com/cache3/89/ql_52404e9c550962bb8b518396b5d24589_l3.png)
+
+    where:
+        - alpha = n_deriv (All Real numbers)
+        - gamma is the gamma function
+        - j! is the factorial of j
+        - h is the step size
+    """
+
+    summation = 0.0
+    y_values = []
+    x_values = np.array(x_values)
+    for i in tqdm.tqdm(range(len(x_values)), desc="Iterating through x values"):
+            summation = 0
+            x = x_values[i]
+            N =  int(x/h)
+            for j in range(N): # Start of summation
+
+                # Computing the Gamma function fraction part of GL formula
+                gamma_top = (math.gamma(n_deriv + 1))
+                gamma_bot = (math.gamma(n_deriv - j + 1))
+                if gamma_bot != 0.0: # Invalid values of gamma func will be 0.0
+                    try:
+                        alpha_j = (gamma_top) / (math.factorial(j) * gamma_bot)
+                    except OverflowError:
+                        # Assume if factorial is too large, it is infinity  
+                        alpha_j = (gamma_top) / (math.inf * gamma_bot)
+                
+                summation += (((-1) ** j) * float(alpha_j) * func(x - (h * j)))
+            
+            y_values.append(((1 / h) ** n_deriv) * summation)
+    return y_values
+
+def C(n, alpha):
+    C_ = (((((-1) ** (n - 1)) * alpha * math.gamma(n - alpha))/((math.gamma(1 - alpha) * math.gamma(n + 1)))) * (1 / math.gamma(n + 1 - alpha)))
+    return C_
+
+########## RL Method from https://arxiv.org/pdf/1208.2588.pdf Equation (4)##########
+
+def FracGradApproxRL(inp_, alpha, h , sample_step = 0.01, f_sympy = None):
+    # N = math.ceil(alpha) + 3 # The approximation gets more accuracte as N -> inf
+    N = 10
+    if f_sympy == None:
+        f_sympy = f
+    x_ = symbols('x')
+    f_ = f_sympy(x_)
+    attr = torch.tensor(inp_).clone().detach()
+    attr = np.array(attr)
+    inp = torch.tensor(inp_).clone().detach()
+    inp = np.array(inp)
+    for i in range(len(inp)):
+            a = -h#inp_[i] - 0.00001
+            summation = 0.0
+            t = i * sample_step#inp[i] 
+#            print("inp_[", i, "] = ",inp_[i])
+            for n in range(N):
+#                print(n)
+                C_ = C(n, alpha)
+                dx = diff(f_, x_, n)
+                x_deriv = dx.subs(x_,t)# * 0.000001
+                
+#                print("n = ", n, "  x_deriv = ", x_deriv)
+                
+                summation += C_ * ((t - a) ** (n - alpha)) * (x_deriv)
+            
+            attr[i] = summation
+    return attr.tolist()
+
--- main.py
+++ main.py
@@ -1,37 +1,109 @@
 import gradio as gr
 import helpers as helpers
 import pandas as pd
-
-def generate_plot(function_sel, derive_order, num_samples, sample_step):
-    x, y = helpers.generate_function_data(helpers.FUNCTIONS[function_sel], num_samples, sample_step)
-    df = pd.DataFrame({"x": x, "y": y, "type": function_sel})
-    # Ca
-    for i in range(derive_order):
-        try:
-            y_d = helpers.get_deriv_approx(x, helpers.FUNCTIONS[function_sel], i+1)
-        except ValueError:
-            gr.Warning(f"Derivative order {i+1} invalid for selected function.")
-            break
-        df_y = pd.DataFrame({"x": x, "y": y_d, "type": f"{function_sel} Order ({i+1})"})
-        df = pd.concat([df, df_y])
-
-    return gr.LinePlot.update(df, x="x", y="y", color="type", height=300, width=500, tooltip=["x", "y"], title="Function Values",)
+import numpy as np
+import altair as alt
+alt.data_transformers.disable_max_rows()
 
 def main():
-    with gr.Blocks() as demo:
-        with gr.Column():
-            func_dd = gr.Dropdown(label="Function", choices=list(helpers.FUNCTIONS.keys()), value="x^2")
-            deriv_order_num = gr.Number(label="Orders of Derivative", value=1, precision=0, step=1, minimum=0)
-            num_samp_num = gr.Number(label="Number of Samples", value=300)
-            samp_step_sl = gr.Slider(label="Sample Step", value=0.01, minimum=0.01, maximum=0.1)
-            gen_btn = gr.Button(label="Generate")
-        with gr.Column():
+    with gr.Blocks(theme=gr.themes.Soft()) as demo:
+        with gr.Row():
+            gr.Markdown("# Fractional Derivative Approximation Visualization\n \
+                This demo visualizes the approximation of fractional \
+                derivatives using many of the different approximation \
+                methods.Such as Grünwald-Letnikov (GL) method.\n\n \
+                Start by selecting a function to approximate the \
+                derivative of")
+        with gr.Row():
+            with gr.Column(): # Input components
+
+                func_dd = gr.Dropdown(label="Function", 
+                                      info="Select a function to approximate the derivative of.",
+                                      choices=list(helpers.FUNCTIONS.keys()), 
+                                      value="x^2")
+                deriv_order_num = gr.Number(label="Orders of Derivative",
+                                            info="The order(s) of derivative to approximate.",
+                                            value=1, 
+                                            precision=0, 
+                                            step=1, 
+                                            minimum=0)
+                deriv_frac_num = gr.Number(label="Number of Fractional Derivatives",
+                                           info="The number of fractional derivatives to approximate between each integer order derivatives.",
+                                           value = 3,
+                                           precision=0,
+                                           step=1,
+                                           minimum=0)
+            with gr.Column():
+                num_samp_sl = gr.Slider(label="Number of Samples",
+                                        info="The number of samples to generate.",
+                                        value=300, 
+                                        minimum =10, 
+                                        maximum=2500, 
+                                        precision=0)
+                samp_step_sl = gr.Slider(label="Sample Step",
+                                         info="The step size between each sample.",
+                                         value=0.01, 
+                                         minimum=0.00001,
+                                         maximum=0.1)
+                plot_width_sl = gr.Slider(label="Plot width",
+                                          info="The width of the plot.",
+                                          value=600, 
+                                          minimum =250, 
+                                          maximum=1920, 
+                                          precision=0)
+                plot_height_sl = gr.Slider(label="Plot width",
+                            info="The width of the plot.",
+                            value=400, 
+                            minimum =250, 
+                            maximum=1920, 
+                            precision=0)
+                gen_btn = gr.Button(label="Generate")
+        with gr.Row(): # Output components
             plot = gr.LinePlot()
 
         gen_btn.click(generate_plot, 
-                    inputs=[func_dd, deriv_order_num, num_samp_num, samp_step_sl],
+                    inputs=[func_dd, deriv_order_num, deriv_frac_num, plot_width_sl, plot_height_sl, num_samp_sl, samp_step_sl],
                     outputs=plot)
 
     demo.queue().launch()
+
+def generate_plot(function_sel, derive_order, num_deriv_frac, plot_width, plot_height, num_samples, sample_step, progress=gr.Progress(track_tqdm=True)):
+    x, y = helpers.generate_function_data(helpers.FUNCTIONS[function_sel], num_samples, sample_step)
+    df = pd.DataFrame({"X": x, "Y": y, "Order": function_sel, "Type": "Derivative"})
+    
+    # Generate derivative data
+    for i in progress.tqdm(range(derive_order), desc="Generating integer order derivatives"):
+        # First handle interger order derivatives
+        # Error checking in case the user selects a derivative order that 
+        # is not valid for the selected function
+        try:
+            y_d = helpers.get_deriv_approx(x, helpers.FUNCTIONS[function_sel], i+1)
+
+            # If we got this far, integer order derivatives are valid, so now
+            # do fractional order derivatives between
+            frac_deriv_orders = np.linspace(start = i, 
+                                            stop  = i+1,
+                                            num   = num_deriv_frac+2)[1:-1]
+            for j in progress.tqdm(frac_deriv_orders, desc="Generating fractional order derivatives"):
+                y_d_frac = helpers.get_frac_deriv_approx_GL(x, helpers.FUNCTIONS[function_sel], j, sample_step)
+                df_y = pd.DataFrame({"X": x, "Y": y_d_frac, "Order": f"{function_sel} Order ({j})", "Type": "Derivative (Fractional)"})
+                df = pd.concat([df, df_y]) # Add to DataFrame
+
+        except ValueError:
+            gr.Warning(f"Derivative order {i+1} invalid for selected function.")
+            break
+        
+        df_y = pd.DataFrame({"X": x, "Y": y_d, "Order": f"{function_sel} Order ({i+1})", "Type": "Derivative"})
+        df = pd.concat([df, df_y]) # Add to DataFrame
+    
+    return gr.LinePlot.update(df, 
+                              x="X", 
+                              y="Y", 
+                              color="Order", 
+                              stroke_dash="Type", 
+                              tooltip=["X", "Y", "Order"], 
+                              title="Function Values", 
+                              width=plot_width,
+                              height=plot_height)
 
 main()
